44268427a472b86a887f58a5a5a49064f202939c
bk <bk@bk-2015.home>
2023-09-17 07:37:32 -0600

    bk manually merged encoder handler code from jak

--- code.py
+++ code.py
@@ -17,8 +17,30 @@ knob.extensions.append(media_keys)
 midi_keys = MidiKeys()
 knob.extensions.append(midi_keys)
 
+class MidiCcEncoderHandler(EncoderHandler):
+    def __init__(self):
+        super().__init__()
+        self.midiValues = [6, 29, 43]
+        print("@init self.midiValues " + str([6, 29, 43]))
+
+    def on_move_do(self, keyboard, encoder_id, state):
+        if state['direction'] == -1:  # left
+            incr = -1
+            print("moving left")
+        else:
+            incr = 1
+            print("moving right")
+        self.midiValues[encoder_id] += incr
+        print("on_move_do " + str(encoder_id) + " = " + str(self.midiValues[encoder_id])
+              + " (incr " + str(incr) + ")")
+        KC.MIDI_CC(encoder_id, self.midiValues[encoder_id])
+
+    def on_button_do(self, keyboard, encoder_id, state):
+        print("on_button_do " + str(encoder_id))
+        # do nothing
+
 # Rotary encoders that also acts as keys
-encoder_handler = EncoderHandler()
+encoder_handler = MidiCcEncoderHandler()
 encoder_handler.divisor = 4
 encoder_handler.pins = (
     (board.D1, board.D2, board.D0),
@@ -26,47 +48,15 @@ encoder_handler.pins = (
     (board.D7, board.D8, board.D6),
 )
 
-global bkMidiValue
-bkMidiValue = [6, 29, 43]
-
-def bkMidiIncrement(thisMidiChannel):
-    print("midi CC + " + str(thisMidiChannel) + ":" + str(thisMidiChannel))
-    bkMidiValue[thisMidiChannel] = bkMidiValue[thisMidiChannel] + 1
-    return bkMidiValue[thisMidiChannel]
-
-def bkMidiDecrement(thisMidiChannel):
-    print("midi CC - " + str(thisMidiChannel) + ":" + str(thisMidiChannel))
-    bkMidiValue[thisMidiChannel] = bkMidiValue[thisMidiChannel] + 1
-#    return bkMidiValue[thisMidiChannel]
-    return 8
-
-def bkMidiIncrement(thisMidiChannel):
-    print("midi CC + " + str(thisMidiChannel) + ":" + str(bkMidiValue[thisMidiChannel]))
-    bkMidiValue[thisMidiChannel] = bkMidiValue[thisMidiChannel] + 1
-    return bkMidiValue[thisMidiChannel]
-
-def bkMidiDecrement(thisMidiChannel):
-    print("midi CC - " + str(thisMidiChannel) + ":" + str(bkMidiValue[thisMidiChannel]))
-    bkMidiValue[thisMidiChannel] = bkMidiValue[thisMidiChannel] - 1
-    return bkMidiValue[thisMidiChannel]
-
-encoder_handler.map = (
-    ((KC.MIDI_CC(0,bkMidiIncrement(0)), KC.MIDI_CC(0,bkMidiDecrement(0)), KC.N0), (KC.MIDI_CC(1,bkMidiIncrement(1)), KC.MIDI_CC(1,bkMidiDecrement(1)), KC.N0), (KC.MIDI_CC(2,bkMidiIncrement(2)), KC.MIDI_CC(2,bkMidiDecrement(2)), KC.VOLD)),
-)
-
 knob.modules.append(encoder_handler)
 
 print('ANAVI Knobs 3')
 
-print(bkMidiValue[0])
-print(bkMidiValue[1])
-print(bkMidiValue[2])
-
 rgb_ext = RGB(
     pixel_pin=board.NEOPIXEL,
     num_pixels=1,
-    val_limit=100,
-    val_default=25,
+    val_limit=15,
+    val_default=15,
     animation_mode=AnimationModes.RAINBOW,
 )
 knob.extensions.append(rgb_ext)
