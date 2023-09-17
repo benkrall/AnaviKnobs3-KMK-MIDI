42e37b50568d3c7c6525120ba26a478961790d31
bk <bk@bk-2015.home>
2023-09-17 09:16:31 -0600

    bk custom MidiCcEncoderHandler works but will not send MIDI_CC

--- code.py
+++ code.py
@@ -15,13 +15,20 @@ media_keys = MediaKeys()
 knob.extensions.append(media_keys)
 
 midi_keys = MidiKeys()
-knob.extensions.append(midi_keys)
+knob.modules.append(midi_keys)
 
 class MidiCcEncoderHandler(EncoderHandler):
     def __init__(self):
         super().__init__()
         self.midiValues = [6, 29, 43]
         print("@init self.midiValues " + str([6, 29, 43]))
+        """self.map = (
+            (0, 0, 0), (0, 0, 0), (0, 0, 0),
+        )"""
+
+        self.map = (
+            ((KC.MIDI_CC(0, 0), KC.MIDI_CC(0, 1), KC.MIDI_CC(0, 2)), (KC.MIDI_CC(1, 0), KC.MIDI_CC(1, 1), KC.MIDI_CC(1, 2)), (KC.MIDI_CC(2, 0), KC.MIDI_CC(2, 1), KC.MIDI_CC(2, 2))),
+        )
 
     def on_move_do(self, keyboard, encoder_id, state):
         if state['direction'] == -1:  # left
@@ -31,7 +38,12 @@ class MidiCcEncoderHandler(EncoderHandler):
             incr = 1
             print("moving right")
         self.midiValues[encoder_id] += incr
-        print("on_move_do " + str(encoder_id) + " = " + str(self.midiValues[encoder_id])
+        if self.midiValues[encoder_id] < 0:
+            self.midiValues[encoder_id] = 0  # limit low value to 0
+        if self.midiValues[encoder_id] > 127:
+            self.midiValues[encoder_id] = 127  # limit high value to 127
+        print("@handler self.midiValues " + str(self.midiValues))
+        print("on_move_do control " + str(encoder_id) + " value " + str(self.midiValues[encoder_id])
               + " (incr " + str(incr) + ")")
         KC.MIDI_CC(encoder_id, self.midiValues[encoder_id])
 
@@ -44,14 +56,20 @@ encoder_handler = MidiCcEncoderHandler()
 encoder_handler.divisor = 4
 encoder_handler.pins = (
     (board.D1, board.D2, board.D0),
-    (board.D9, board.D10, board.D3),
-    (board.D7, board.D8, board.D6),
+    (board.D10, board.D9, board.D3),
+    (board.D8, board.D7, board.D6),
+)
+
+# what is in the map does not seem to matter, each encoder just needs a map
+"""
+encoder_handler.map = (
+    (0, 0, 0), (0, 0, 0), (0, 0, 0),
 )
 
 encoder_handler.map = (
-    (KC.N0, KC.N0, KC.N0), (KC.N0, KC.N0, KC.N0), (KC.N0, KC.N0, KC.N0),
+    ((KC.MIDI_CC(0, 0), KC.MIDI_CC(0, 1), KC.MIDI_CC(0, 2)), (KC.MIDI_CC(1, 0), KC.MIDI_CC(1, 1), KC.MIDI_CC(1, 2)), (KC.MIDI_CC(2, 0), KC.MIDI_CC(2, 1), KC.MIDI_CC(2, 2))),
 )
-
+"""
 
 knob.modules.append(encoder_handler)
 
@@ -66,7 +84,7 @@ rgb_ext = RGB(
 )
 knob.extensions.append(rgb_ext)
 
-knob.keymap = [[KC.MIDI_CC]]
+knob.keymap = [[KC.N0]]
 
 if __name__ == '__main__':
     knob.go()
